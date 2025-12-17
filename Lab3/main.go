eb69f92c5475ec3e80f0c60e8ef6b6bc898ac4bb
AymaRehman <ayma.rehman24@gmail.com>
2025-12-17 21:38:09 +0200

    Initial project setup with main and required header comments

---
+++ Lab3/main.go
@@ -0,0 +1,13 @@
+// Full Name: Ayma Rehman
+// Student ID: 241ADB165
+// Course: DE0917 - Programming Languages
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("=================================")
+	fmt.Println("Gosort – concurrent chunk sorting")
+	fmt.Println("=================================")
+}
